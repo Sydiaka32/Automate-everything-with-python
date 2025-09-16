32c49205a49dc1943eb39df5efb6d39bb8db4841
Sydiaka32 <max.sydiaka@gmail.com>
2025-09-16 22:06:27 +0300

    Scraping dynamic value

--- Browser Automation and Web Scraping/Scraping Simple text with Selenium.py
+++ Browser Automation and Web Scraping/Scraping Simple text with Selenium.py
@@ -1,5 +1,5 @@
 from selenium import webdriver
-
+import time
 def get_driver():
     # Set options to make browsing easier
     options = webdriver.ChromeOptions()
@@ -14,10 +14,15 @@ def get_driver():
     driver.get("http://automated.pythonanywhere.com")
     return driver
 
+def clean_text(text):
+    """Extract only the temperature from text"""
+    output = float(text.split(": ")[1])
+    return output
 
 def main():
     driver = get_driver()
-    element = driver.find_element(by="xpath", value="/html/body/div[1]/div/h1[1]")
-    return element
+    time.sleep(2)
+    element = driver.find_element(by="xpath", value="/html/body/div[1]/div/h1[2]")
+    return clean_text(element.text)
 
 print(main())
